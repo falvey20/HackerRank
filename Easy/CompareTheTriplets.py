1eb031d72f1afb2e6470ee9f58c1244a2b34f41b
falvey20 <69197089+falvey20@users.noreply.github.com>
2021-07-01 18:09:58 +0100

    Create CompareTheTriplets.py

---
+++ Easy/CompareTheTriplets.py
@@ -0,0 +1,40 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def compareTriplets(a, b):
+    a_total = 0
+    b_total = 0
+    for i in range(3):
+        if a[i] > b[i]:
+            a_total += 1
+        elif a[i] < b[i]:
+            b_total += 1
+        
+    return [a_total, b_total]
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
