ae0177374424e7586ead645417f54af2deddf327
falvey20 <69197089+falvey20@users.noreply.github.com>
2021-08-17 21:47:17 +0100

    Create StringValidators.py

---
+++ Easy/StringValidators.py
@@ -0,0 +1,44 @@
+if __name__ == '__main__':
+    s = input()
+
+def fun1(s):
+    for i in range(len(s)):
+        if(s[i].isalnum()):
+            return True;
+            break;
+    return False;
+        
+def fun2(s):
+    for i in range(len(s)):
+        if(s[i].isalpha()):
+            return True;
+            break;
+    return False;
+
+def fun3(s):
+    for i in range(len(s)):
+        if(s[i].isdigit()):
+            return True;
+            break;
+    return False;
+
+def fun4(s):
+    for i in range(len(s)):
+        if(s[i].islower()):
+            return True;
+            break;
+    return False; 
+     
+def fun5(s):
+    for i in range(len(s)):
+        if(s[i].isupper()):
+            return True;
+            break;
+    return False;
+
+
+print(fun1(s))
+print(fun2(s))
+print(fun3(s))
+print(fun4(s))
+print(fun5(s))
