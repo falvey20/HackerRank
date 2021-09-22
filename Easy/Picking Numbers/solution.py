e8dda43fb465782e23565a98c333fb4616e819b5
falvey20 <69197089+falvey20@users.noreply.github.com>
2021-09-22 16:28:22 +0100

    Create solution.py

---
+++ Easy/Picking Numbers/solution.py
@@ -0,0 +1,38 @@
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    maximum = 0
+    
+    counts = Counter(a)
+    for i in sorted(counts):
+        curr_max = counts[i] + counts.get(i+1, 0)
+        if curr_max > maximum:
+            maximum = curr_max
+    return maximum
+        
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
