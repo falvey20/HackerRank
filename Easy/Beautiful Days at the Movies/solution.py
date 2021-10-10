5c85487f96adbd65ac525b3a7a07fb9a30f33039
falvey20 <69197089+falvey20@users.noreply.github.com>
2021-10-10 20:25:50 +0100

    Create solution.py

---
+++ Easy/Beautiful Days at the Movies/solution.py
@@ -0,0 +1,42 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulDays' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER i
+#  2. INTEGER j
+#  3. INTEGER k
+#
+
+def beautifulDays(i, j, k):
+    beaut_days = 0
+    
+    for num in range(i, j+1):
+        new_num = int(str(num)[::-1])
+        difference = abs(num - new_num)
+        if difference % k == 0:
+            beaut_days += 1
+    return beaut_days
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    i = int(first_multiple_input[0])
+
+    j = int(first_multiple_input[1])
+
+    k = int(first_multiple_input[2])
+
+    result = beautifulDays(i, j, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
