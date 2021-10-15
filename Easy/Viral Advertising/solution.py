7300f8f385f2f38e8718724d4915ff39ab27114c
falvey20 <69197089+falvey20@users.noreply.github.com>
2021-10-15 12:00:35 +0100

    Create solution.py

---
+++ Easy/Viral Advertising/solution.py
@@ -0,0 +1,33 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    cumulative = 0
+    shared = 5
+    for day in range(1, n+1):
+        cumulative += shared//2
+        shared = (shared//2)*3
+    return cumulative
+        
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
