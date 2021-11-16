7f0497ae287863c0b1dd361498e4aca03981f4f6
falvey20 <69197089+falvey20@users.noreply.github.com>
2021-11-16 14:15:31 +0000

    Create solution.py

---
+++ Easy/Beautiful Triplets/solution.py
@@ -0,0 +1,41 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulTriplets' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def beautifulTriplets(d, arr):
+    count = 0
+    l = len(arr)
+    for i in range(0, l-d):
+        # Establish if arr contains values that are d and (d+d) greater than it.
+        if arr[i]+d in arr and arr[i]+(d*2) in arr:
+            count+=1
+    return count
+        
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = beautifulTriplets(d, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
