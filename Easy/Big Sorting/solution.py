90088805ea6118936569ddfbd3fe61828083aa04
falvey20 <69197089+falvey20@users.noreply.github.com>
2021-12-06 12:20:14 +0000

    Create solution.py

---
+++ Easy/Big Sorting/solution.py
@@ -0,0 +1,37 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bigSorting' function below.
+#
+# The function is expected to return a STRING_ARRAY.
+# The function accepts STRING_ARRAY unsorted as parameter.
+#
+
+def bigSorting(unsorted):
+    # converting to int, sorting, then converting back to string is too slow.
+    # sort each x by len(x) if not value.
+    unsorted.sort(key=lambda x: (len(x), x))
+    
+    return unsorted
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    unsorted = []
+
+    for _ in range(n):
+        unsorted_item = input()
+        unsorted.append(unsorted_item)
+
+    result = bigSorting(unsorted)
+
+    fptr.write('\n'.join(result))
+    fptr.write('\n')
+
+    fptr.close()
