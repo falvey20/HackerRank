e1bde21fa11efc72ed2bd8f0a2fb1061653fe607
falvey20 <69197089+falvey20@users.noreply.github.com>
2021-12-09 14:58:50 +0000

    Create solution.py

---
+++ Easy/CamelCase/solution.py
@@ -0,0 +1,30 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'camelcase' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def camelcase(s):
+    words = 0
+    for char in s:
+        if char.isupper():
+            words += 1
+    return words + 1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
