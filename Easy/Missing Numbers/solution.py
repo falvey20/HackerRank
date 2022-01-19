06779464f4c40c402aea4268570f64d860065238
falvey20 <69197089+falvey20@users.noreply.github.com>
2022-01-19 09:11:36 +0000

    Create solution.py

---
+++ Easy/Missing Numbers/solution.py
@@ -0,0 +1,41 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'missingNumbers' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY arr
+#  2. INTEGER_ARRAY brr
+#
+
+def missingNumbers(arr, brr):
+    result = []
+    
+    for i in set(brr):
+        if brr.count(i) > arr.count(i):
+            result.append(i)
+            
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    m = int(input().strip())
+
+    brr = list(map(int, input().rstrip().split()))
+
+    result = missingNumbers(arr, brr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
