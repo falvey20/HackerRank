4384b0906206aed94521289170fdaca9ae024d55
falvey20 <69197089+falvey20@users.noreply.github.com>
2022-01-28 11:49:04 +0000

    Create solution.py

---
+++ Collection.namedtuple()/solution.py
@@ -0,0 +1,7 @@
+from collections import namedtuple
+
+N, headers, total = int(input()), list(input().split()), 0
+
+for i in range(N):
+    total += int(list(input().split())[headers.index('MARKS')])
+print(total/N)
