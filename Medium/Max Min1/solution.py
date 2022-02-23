09f37f2029aa9bc5f43e7e338b54520620ee90af
falvey20 <69197089+falvey20@users.noreply.github.com>
2022-02-23 12:10:07 +0000

    Create solution.py

---
+++ Medium/Max Min1/solution.py
@@ -0,0 +1,44 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maxMin' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY arr
+#
+
+def maxMin(k, arr):
+    arr = sorted(arr)
+    result = arr[(k-1)]-arr[0]
+    
+    for i in range(len(arr)-(k-1)):
+        diff = abs(arr[i+(k-1)] - arr[i])
+        if diff < result:
+            result = diff
+            
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    k = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = int(input().strip())
+        arr.append(arr_item)
+
+    result = maxMin(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
