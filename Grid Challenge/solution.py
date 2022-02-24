9a75adb72e2da8efa3fd961e92ea95c8dcd7dac3
falvey20 <69197089+falvey20@users.noreply.github.com>
2022-02-24 17:24:05 +0000

    Create solution.py

---
+++ Grid Challenge/solution.py
@@ -0,0 +1,42 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gridChallenge' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING_ARRAY grid as parameter.
+#
+
+def gridChallenge(grid):
+    rows = [sorted(row) for row in grid]
+    
+    for i in range(0, len(rows[0])):
+        for row in range(0, len(rows)-1):
+            if rows[row+1][i] < rows[row][i]:
+                return 'NO'
+    return 'YES'
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        grid = []
+
+        for _ in range(n):
+            grid_item = input()
+            grid.append(grid_item)
+
+        result = gridChallenge(grid)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
