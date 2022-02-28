a9974994c382d26589c477c0a62995fb5e64a340
falvey20 <69197089+falvey20@users.noreply.github.com>
2022-02-28 16:48:42 +0000

    Create solution.py

---
+++ Easy/Sherlock and Array/solution.py
@@ -0,0 +1,41 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'balancedSums' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def balancedSums(arr):
+    s = sum(arr)
+    left_tot = 0
+    
+    for i in range(len(arr)):
+        if 2*left_tot == s-arr[i]:
+            return 'YES'
+        else:
+            left_tot += arr[i]
+    return 'NO'
+        
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = balancedSums(arr)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
