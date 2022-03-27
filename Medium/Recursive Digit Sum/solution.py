77ed8d1d0fd2e657f11f75aa10ecd519aa143f3f
falvey20 <69197089+falvey20@users.noreply.github.com>
2022-03-27 17:17:00 +0100

    Create solution.py

---
+++ Medium/Recursive Digit Sum/solution.py
@@ -0,0 +1,35 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'superDigit' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING n
+#  2. INTEGER k
+#
+
+def superDigit(n, k):
+    p = str(sum(int(i) for i in n)*k)
+    while len(p) != 1:
+        p = str(sum(int(i) for i in p))
+    return int(p)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = first_multiple_input[0]
+
+    k = int(first_multiple_input[1])
+
+    result = superDigit(n, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
