99fe1b0f9e27c301ff6100305432f0bde660844c
falvey20 <69197089+falvey20@users.noreply.github.com>
2022-04-06 10:45:04 +0100

    Create solution.py

---
+++ Easy/Equal Stacks/solution.py
@@ -0,0 +1,48 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'equalStacks' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h1
+#  2. INTEGER_ARRAY h2
+#  3. INTEGER_ARRAY h3
+#
+
+def equalStacks(h1, h2, h3):
+    stacks = [h1, h2, h3]
+    s = list(map(sum, stacks))
+    while len(set(s)) != 1:
+        tallest = s.index(max(s))
+        first_ele = stacks[tallest].pop(0)
+        s[tallest] -= first_ele
+
+    return sum(stacks[0])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n1 = int(first_multiple_input[0])
+
+    n2 = int(first_multiple_input[1])
+
+    n3 = int(first_multiple_input[2])
+
+    h1 = list(map(int, input().rstrip().split()))
+
+    h2 = list(map(int, input().rstrip().split()))
+
+    h3 = list(map(int, input().rstrip().split()))
+
+    result = equalStacks(h1, h2, h3)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
