a399eb34abed2eb4faf78a9e01df8bca87fa0835
falvey20 <69197089+falvey20@users.noreply.github.com>
2022-05-06 14:25:12 +0100

    Create solution.py

---
+++ Medium/Candies/solution.py
@@ -0,0 +1,42 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'candies' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
+def candies(n, arr):
+    result = [1]*n
+    for i in range(1, n):
+        if arr[i] > arr[i-1]:
+            result[i] = result[i-1] + 1
+    for j in range(len(arr)-1, 0, -1):
+        if arr[j-1] > arr[j] and result[j-1] <= result[j]:
+            result[j-1] = result[j]+1
+    # print(result)
+    return sum(result)
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = int(input().strip())
+        arr.append(arr_item)
+
+    result = candies(n, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
