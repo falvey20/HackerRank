db91c5d414d34736118b5d02c4ad7e7a0f080223
falvey20 <69197089+falvey20@users.noreply.github.com>
2022-07-01 15:21:00 +0100

    Create solution.py

---
+++ Easy/Maximizing XOR/solution.py
@@ -0,0 +1,35 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maximizingXor' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER l
+#  2. INTEGER r
+#
+
+def maximizingXor(l, r):
+    result = 0
+    for i in range(l, r+1):
+        for j in range(l, r+1):
+            if i ^ j > result:
+                result = i ^ j
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    l = int(input().strip())
+
+    r = int(input().strip())
+
+    result = maximizingXor(l, r)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
