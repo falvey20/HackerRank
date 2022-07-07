058d89d8a951a49dac559c3fe6187fe48e7697b9
falvey20 <69197089+falvey20@users.noreply.github.com>
2022-07-07 14:03:13 +0100

    Create solution.py

---
+++ Medium/Larry's Array/solution.py
@@ -0,0 +1,37 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'larrysArray' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY A as parameter.
+#
+
+def larrysArray(A):
+    inversions = 0
+    for i in range(1, len(A)):
+        inversions += sum([1 for x in A[0:i] if x > A[i]])
+        
+    if inversions % 2 == 0:
+        return 'YES'
+    return 'NO'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        A = list(map(int, input().rstrip().split()))
+
+        result = larrysArray(A)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
